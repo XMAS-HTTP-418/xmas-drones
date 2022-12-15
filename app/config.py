53168fb5038903cb03f6cc94def56f31744cef5f
pavivin <pavivin@yandex.ru>
2022-12-16 03:00:28 +0400

    Image service

--- app/config.py
+++ app/config.py
@@ -1,1 +1,8 @@
+import numpy as np
+
+
 MIN_COLOR_VALUE = 255 * 255 * 255
+MISSION_AREA_IMAGE = 'data/mission_area.png'
+MISSION_TARGETS_IMAGE = 'data/mission_targets.png'
+WHITE_COLOR = np.array([255, 255, 255], dtype='uint8')
+BLACK_COLOR = np.array([0, 0, 0], dtype='uint8')
