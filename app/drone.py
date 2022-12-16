78f4d1321faa16676b6a0026f91dbf9af64b135a
huscker <egostalker03@mail.ru>
2022-12-16 03:29:39 +0300

    Слинковал код

--- app/drone.py
+++ app/drone.py
@@ -4,6 +4,8 @@ from mission import Mission
 from station import Station
 from load import LoadType, Load
 from dataclasses import dataclass
+from dijkstra import Dijkstra, getArrayHeightMap
+from data_parser import DataParser
 
 @dataclass
 class Drone:
@@ -22,13 +24,10 @@ class Drone:
     load: Optional[Load]
 
     def calculate_energy_for_flying(self, start_position: np.array, end_position: np.array) -> float:
-        pass
-
-    def get_closest_station(self) -> Station:
-        pass
-
-    def get_closest_station_with_load_type(self, load_type: LoadType) -> Station:
-        pass
+        heightmap = getArrayHeightMap('data/height_map.png')
+        pathfinder = Dijkstra(heightmap, start=(int(start_position[0]), int(start_position[1])))
+        distance = pathfinder.getDistances((int(end_position[0]), int(end_position[1])))
+        return distance*self.power
 
     def evaluate_mission_cost(self, mission: Mission) -> np.float:
         additional_cost = 0.0
@@ -36,10 +35,9 @@ class Drone:
             if self.load.type == LoadType[mission.type]:
                 additional_cost = 0.0
             else: # return to station to store and pickup
-                additional_cost = self.calculate_energy_for_flying(self.position, self.get_closest_station())
-                additional_cost += self.calculate_energy_for_flying(self.get_closest_station(), self.get_closest_station_with_load_type(LoadType[mission.type]))
+                additional_cost += self.calculate_energy_for_flying(self.position, DataParser.get_closest_station_by_load_type(self,LoadType[mission.type]))
         else: # return to station to pickup
-            additional_cost = self.calculate_energy_for_flying(self.position, self.get_closest_station_with_load_type(LoadType[mission.type]))
+            additional_cost = self.calculate_energy_for_flying(self.position, DataParser.get_closest_station_by_load_type(self,LoadType[mission.type]))
         flying_to_mission_cost = self.calculate_energy_for_flying(self.position, mission.get_closest_position())
         return additional_cost + flying_to_mission_cost
 
