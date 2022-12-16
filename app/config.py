d37f26fdd0c922c0abd5d64c645ff1597bc8df5c
pavivin <pavivin@yandex.ru>
2022-12-16 13:33:39 +0400

    master slave

--- app/config.py
+++ app/config.py
@@ -7,3 +7,6 @@ TARGETS_SCANNING_IMAGE = 'data/targets_scanning.png'
 TARGETS_POLLINATION_IMAGE = 'data/targets_pollinating.png'
 WHITE_COLOR = np.array([255, 255, 255], dtype='uint8')
 BLACK_COLOR = np.array([0, 0, 0], dtype='uint8')
+
+SOCKET_HOST = "127.0.0.1"  # The server's hostname or IP address
+SOCKET_PORT = 65432  # The port used by the server
--- app/slave.py
+++ app/slave.py
@@ -2,11 +2,10 @@
 
 import socket
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+from app.config import SOCKET_HOST, SOCKET_PORT
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+    s.connect((SOCKET_HOST, SOCKET_PORT))
     s.sendall(b"Hello, world")
     data = s.recv(1024)
 
